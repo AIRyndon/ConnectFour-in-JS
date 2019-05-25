878152254ca861518c7b898d8e90d3cf8dce8a4e
Angelo I Ryndon <angelo@angeloryndon.com>
2019-05-25 17:28:09 +1200

    Add function to fill out nodes in the board

--- ConnectFour-in-JS/wwwroot/js/site.js
+++ ConnectFour-in-JS/wwwroot/js/site.js
@@ -3,40 +3,83 @@
 
 // Write your Javascript code.
 
-var board = [];
+$(function () {
 
-function InitialBoard(board, row, column) {
+    var board = [];
+    var players = {
+        playerOne: 'player-one',
+        playerTwo: 'player-two'
+    };
 
-    board[column][row] = new Node(row, column);
-    var node = FillNode(board[column][row], 0, 'white');
-    console.log(node);
-}
+    var playerTurn = players.playerOne;
 
-function LoopThroughBoard(board, callback) {
+    function InitiateBoard(board, row, column) {
 
-    for (var column = 0; column < 7; column++) {
-        board[column] = [];
+        board[column][row] = new Node(row, column);
+        var node = FillNode(board[column][row], false, 'white');
+    }
 
-        for (var row = 0; row < 6; row++) {
-            InitialBoard(board, row, column);
+    function LoopThroughBoard(board, callback) {
+
+        for (var column = 0; column < 7; column++) {
+            board[column] = [];
+
+            for (var row = 0; row < 6; row++) {
+                callback(board, row, column);
+            }
         }
     }
-}
 
-function FillNode(node, isFilled, color) {
+    function FillNode(node, isFilled, color) {
 
-    node.isFilled = isFilled;
-    node.color = color;
+        node.isFilled = isFilled;
+        node.color = color;
 
-    return node;
-}
+        return node;
+    }
 
-function Node(row, column) {
+    function Node(row, column) {
 
-    this.row = row;
-    this.column = column;
-    this.isFilled;
-    this.color;
-}
+        this.row = row;
+        this.column = column;
+        this.isFilled;
+        this.color;
+    }
 
-LoopThroughBoard(board,InitialBoard);
+    function UpdateBoardState(event) {
+
+        var button = this;
+        console.log(board);
+        for (var row = 5; row >= 0; row--) {
+            var node = board[row][button.id];
+
+            if (!node.isFilled) {
+
+                FillNode(node, true);
+                var elem = $('#col' + button.id + 'row' + row).addClass(playerTurn);
+
+                if (playerTurn === players.playerOne) {
+                    playerTurn = players.playerTwo;
+                }
+                else {
+                    playerTurn = players.playerOne;
+                }
+                break;
+            }
+        }
+    }
+
+    //############################################################
+    //Initial board
+
+    LoopThroughBoard(board, InitiateBoard);
+
+
+    //##############################################
+    //Register click events
+    var buttons = document.querySelectorAll('button.column');
+    console.log(buttons);
+    for (var index = 0; index < buttons.length; index++) {
+        buttons[index].addEventListener('click', UpdateBoardState);
+    }
+});
