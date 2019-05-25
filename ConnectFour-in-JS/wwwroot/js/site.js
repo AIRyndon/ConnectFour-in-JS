be2febe9400c8ef362b8832b497b572d7f0fa4f6
Angelo I Ryndon <angelo@angeloryndon.com>
2019-05-25 18:59:38 +1200

    Implement checking a column for a winner

--- ConnectFour-in-JS/wwwroot/js/site.js
+++ ConnectFour-in-JS/wwwroot/js/site.js
@@ -6,36 +6,38 @@
 $(function () {
 
     var board = [];
+    var turns = 0;
+    var winner = 0;
     var players = {
         playerOne: 'player-one',
         playerTwo: 'player-two'
     };
 
     var playerTurn = players.playerOne;
+    var currentNode;
 
     function InitiateBoard(board, row, column) {
 
-        board[column][row] = new Node(row, column);
-        var node = FillNode(board[column][row], false, 'white');
+        board[row][column] = new Node(row, column);
+        var node = FillNode(board[row][column], false, 'blank');
     }
 
     function LoopThroughBoard(board, callback) {
 
-        for (var column = 0; column < 7; column++) {
-            board[column] = [];
+        for (var row = 0; row < 6; row++) {
+            board[row] = [];
 
-            for (var row = 0; row < 6; row++) {
+            for (var column = 0; column < 7; column++) {
                 callback(board, row, column);
             }
         }
+        console.log(board);
     }
 
-    function FillNode(node, isFilled, color) {
+    function FillNode(node, isFilled, piece) {
 
         node.isFilled = isFilled;
-        node.color = color;
-
-        return node;
+        node.piece = piece;
     }
 
     function Node(row, column) {
@@ -43,7 +45,45 @@ $(function () {
         this.row = row;
         this.column = column;
         this.isFilled;
-        this.color;
+        this.piece;
+    }
+
+    function CheckColumn(board, node) {
+        var count = 0;
+        var lastDropped = 1;
+
+        count = CheckDownward(board, node, count);
+
+        return lastDropped + count;
+    }
+
+    function CheckDownward(board, node, count) {
+
+        var rowDown;
+        if (node.row < 5 && count < 3) {
+            rowDown = board[node.row + 1][node.column];
+
+            if (node.piece === rowDown.piece) {
+                ++count;
+
+                return CheckDownward(board, rowDown, count);
+            }
+        }
+    
+        return count;
+    }
+
+    function CheckWinner(board, node) {
+
+        console.log(board);
+        console.log(node);
+
+        var connectFour = 0;
+        var gameWon = 0;
+
+        connectFour = CheckColumn(board, node);
+
+        console.log(connectFour);
     }
 
     function UpdateBoardState(event) {
@@ -51,11 +91,13 @@ $(function () {
         var button = this;
         console.log(board);
         for (var row = 5; row >= 0; row--) {
+            console.log(board[row][button.id]);
             var node = board[row][button.id];
-
+            
             if (!node.isFilled) {
-
-                FillNode(node, true);
+                currentNode = node;
+                ++turns;
+                FillNode(node, true,playerTurn);
                 var elem = $('#col' + button.id + 'row' + row).addClass(playerTurn);
 
                 if (playerTurn === players.playerOne) {
@@ -64,10 +106,24 @@ $(function () {
                 else {
                     playerTurn = players.playerOne;
                 }
+                console.log(turns);
+
                 break;
             }
         }
+
+        if (turns > 6) {
+            console.log('At turns');
+            console.log(turns);
+            CheckWinner(board, currentNode);
+        }    
     }
+
+    
+
+    //##############Checking functions######################
+
+
 
     //############################################################
     //Initial board
@@ -82,4 +138,6 @@ $(function () {
     for (var index = 0; index < buttons.length; index++) {
         buttons[index].addEventListener('click', UpdateBoardState);
     }
+
+    //#########################################
 });
