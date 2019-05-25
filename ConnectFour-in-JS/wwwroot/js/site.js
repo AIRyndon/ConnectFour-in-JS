552b57099f79ae54a7011c58c1c81bb398388e79
Angelo I Ryndon <angelo@angeloryndon.com>
2019-05-25 15:43:15 +1200

    Add functions to create initial board state

--- ConnectFour-in-JS/wwwroot/js/site.js
+++ ConnectFour-in-JS/wwwroot/js/site.js
@@ -3,4 +3,40 @@
 
 // Write your Javascript code.
 
+var board = [];
 
+function InitialBoard(board, row, column) {
+
+    board[column][row] = new Node(row, column);
+    var node = FillNode(board[column][row], 0, 'white');
+    console.log(node);
+}
+
+function LoopThroughBoard(board, callback) {
+
+    for (var column = 0; column < 7; column++) {
+        board[column] = [];
+
+        for (var row = 0; row < 6; row++) {
+            InitialBoard(board, row, column);
+        }
+    }
+}
+
+function FillNode(node, isFilled, color) {
+
+    node.isFilled = isFilled;
+    node.color = color;
+
+    return node;
+}
+
+function Node(row, column) {
+
+    this.row = row;
+    this.column = column;
+    this.isFilled;
+    this.color;
+}
+
+LoopThroughBoard(board,InitialBoard);
